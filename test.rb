dc505056c4176f09b071d136bd1aad70dd9ae485
David Litvak <david.litvakb@gmail.com>
2012-03-13 20:53:43 -0300

    calendar kata

--- test.rb
+++ test.rb
@@ -31,6 +31,7 @@ class TestXXX < Test::Unit::TestCase
   def test_6
     @calendar.add_non_working_day(Time.new Time.now.year, 1, 1)
     @calendar.add_non_working_day(Time.new Time.now.year, 2, 3)
+    @calendar.add_non_working_day(:sunday)
     assert @calendar.is_non_working_date(Time.new 2012, 1, 1)
     assert @calendar.is_non_working_date(Time.new 2012, 2, 3)
   end
@@ -38,6 +39,7 @@ class TestXXX < Test::Unit::TestCase
   def test_7
     @calendar.add_non_working_day(Time.new Time.now.year, 1, 1)
     @calendar.add_non_working_day(Time.new Time.now.year, 2, 3)
+    @calendar.add_non_working_day(:tuesday)
     assert !@calendar.is_non_working_date(Time.new 2012, 4, 1)
   end
 end
