b5284089bf6ea031ba5ad86489b4dc0c2c6e2c1e
David Litvak <david.litvakb@gmail.com>
2012-03-13 22:36:25 -0300

    removed constructor duplication

--- working_date_calendar.rb
+++ working_date_calendar.rb
@@ -14,21 +14,19 @@ class WorkingDayCalendar
   end
 end
 
-class NonWorkingDayOfMonth
+class NonWorkingDayRule
   def initialize day
     @day = day
   end
+end
 
+class NonWorkingDayOfMonth < NonWorkingDayRule
   def is_non_working_day day
     @day.day.eql?(day.day) and @day.month.eql?(day.month)
   end
 end
 
-class NonWorkingDayOfWeek
-  def initialize day
-    @day = day
-  end
-
+class NonWorkingDayOfWeek < NonWorkingDayRule
   def is_non_working_day day
     day.send "#{@day}?"
   end
