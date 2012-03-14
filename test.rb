2fe40feb9ad78c4503f5d5c2b00748c2a75daee7
David Litvak <david.litvakb@gmail.com>
2012-03-13 22:27:39 -0300

    reified objects, now there are rules

--- test.rb
+++ test.rb
@@ -1,45 +1,58 @@
 require 'test/unit'
 require './working_date_calendar'
 
-class TestXXX < Test::Unit::TestCase
+module ConstantDates
+  def ConstantDates.some_sunday; Time.new 2012, 03, 11; end
+  def ConstantDates.some_monday; Time.new 2012, 03, 12; end
+  def ConstantDates.january_first; Time.new 2012, 01, 01; end
+end
+
+class WorkingDayCalendarTest < Test::Unit::TestCase
   def setup
     @calendar = WorkingDayCalendar.new
-    @some_sunday = Time.new 2012, 03, 11
-    @some_monday = Time.new 2012, 03, 12
     @some_saturday = Time.new 2012, 03, 10
     @some_tuesday = Time.new 2012, 03, 13
   end
 
-  def test_when_calendar_has_a_non_working_day_of_week_return_non_working_for_that_day_of_week
-    @calendar.add_non_working_day(:sunday)
-    assert @calendar.is_non_working_date(@some_sunday)
+  def test_non_working_day_of_month_rule
+    @calendar.add_non_working_day_rule(
+      NonWorkingDayOfMonth.new Time.new(Time.now.year, 1, 1)
+    )
+    assert @calendar.is_non_working_day(ConstantDates::january_first)
+    assert !@calendar.is_non_working_day(ConstantDates::some_monday)
   end
 
-  def test_when_calendar_has_a_non_working_day_of_week_return_working_for_other_day_of_week
-    @calendar.add_non_working_day(:sunday)
-    assert !@calendar.is_non_working_date(@some_monday)
+  def test_non_working_day_of_week_rule
+    @calendar.add_non_working_day_rule(NonWorkingDayOfWeek.new :sunday)
+    assert @calendar.is_non_working_day(ConstantDates::some_sunday)
+    assert !@calendar.is_non_working_day(ConstantDates::some_monday)
+  end
+end
+
+class NonWorkingDayOfMonthTest < Test::Unit::TestCase
+  def test_a_non_working_day_of_month_should_respond_that_the_same_day_is_non_working
+    assert NonWorkingDayOfMonth.new(
+      ConstantDates::january_first
+    ).is_non_working_day(ConstantDates::january_first)
   end
 
-  def test_3
-    @calendar.add_non_working_day(:saturday)
-    @calendar.add_non_working_day(:sunday)
-    assert @calendar.is_non_working_date(@some_sunday)
-    assert @calendar.is_non_working_date(@some_saturday)
-    assert !@calendar.is_non_working_date(@some_monday)
+  def test_a_non_working_day_of_month_should_respond_that_other_day_is_working
+    assert !NonWorkingDayOfMonth.new(
+      ConstantDates::january_first
+    ).is_non_working_day(ConstantDates::some_sunday)
+  end
+end
+
+class NonWorkingDayOfWeekTest < Test::Unit::TestCase
+  def test_a_non_working_day_of_week_should_respond_that_the_same_day_is_non_working
+    assert NonWorkingDayOfWeek.new(
+      :sunday
+    ).is_non_working_day(ConstantDates::some_sunday)
   end
 
-  def test_6
-    @calendar.add_non_working_day(Time.new Time.now.year, 1, 1)
-    @calendar.add_non_working_day(Time.new Time.now.year, 2, 3)
-    @calendar.add_non_working_day(:sunday)
-    assert @calendar.is_non_working_date(Time.new 2012, 1, 1)
-    assert @calendar.is_non_working_date(Time.new 2012, 2, 3)
-  end
-
-  def test_7
-    @calendar.add_non_working_day(Time.new Time.now.year, 1, 1)
-    @calendar.add_non_working_day(Time.new Time.now.year, 2, 3)
-    @calendar.add_non_working_day(:tuesday)
-    assert !@calendar.is_non_working_date(Time.new 2012, 4, 1)
+  def test_a_non_working_day_of_week_should_respond_that_other_day_is_working
+    assert !NonWorkingDayOfWeek.new(
+      :sunday
+    ).is_non_working_day(ConstantDates::some_monday)
   end
 end
--- working_date_calendar.rb
+++ working_date_calendar.rb
@@ -1,27 +1,35 @@
 class WorkingDayCalendar
   def initialize
-    @non_working = []
+    @non_working_rules = []
   end
 
-  def is_non_working_date day
-    return false if @non_working.nil?
-    @non_working.any? { |non_working|
-      (is_non_working_day_of_month non_working, day) or
-        (is_non_working_day_of_week non_working, day)
+  def is_non_working_day day
+    @non_working_rules.any? { |rule|
+      rule.is_non_working_day day
     }
   end
 
-  def is_non_working_day_of_month non_working, day
-    return false if !(non_working.respond_to? :day or non_working.respond_to? :month)
-    non_working.day.eql?(day.day) and non_working.month.eql?(day.month)
+  def add_non_working_day_rule rule
+    @non_working_rules << rule
+  end
+end
+
+class NonWorkingDayOfMonth
+  def initialize day
+    @day = day
   end
 
-  def is_non_working_day_of_week non_working, day
-    return false if !(day.respond_to? "#{non_working}?".to_sym)
-    day.send "#{non_working}?"
+  def is_non_working_day day
+    @day.day.eql?(day.day) and @day.month.eql?(day.month)
+  end
+end
+
+class NonWorkingDayOfWeek
+  def initialize day
+    @day = day
   end
 
-  def add_non_working_day day
-    @non_working << day
+  def is_non_working_day day
+    day.send "#{@day}?"
   end
 end
